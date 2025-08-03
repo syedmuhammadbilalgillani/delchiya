6ba470195b4fe2e3b0a7223483c3414159191c08
Syed Muhammad Bilal Gillani <syedmuhammadbilalgillani2003@gmail.com>
2025-08-04 01:08:08 +0500

    Update page.tsx

--- app/(admin)/d/a/page.tsx
+++ app/(admin)/d/a/page.tsx
@@ -1,7 +1,7 @@
 // app/bookings/page.tsx
 import { BookingsTable } from "@/components/booking-table";
 import prisma from "@/lib/prisma";
-
+export const dynamic = "force-dynamic"; // Ensure this page is always dynamic
 const Page = async () => {
   const bookings = await prisma.booking.findMany();
   return <BookingsTable initialBookings={bookings} />;
